71ab9ff9864b46ed22ab47b070ac10eacb827437
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-04 11:16:16 +0000

    Add fuel.py file

---
+++ week-3/fuel/fuel.py
@@ -0,0 +1,30 @@
+while True:
+    try:
+        print("Please enter a proper fraction or press Ctrl + C to exit program")
+        user_input = input("Fraction: ")
+        user_input = user_input.split("/")
+        x, y = int(user_input[0]), int(user_input[1])
+
+    except ValueError:
+        pass
+
+    except ZeroDivisionError:
+        # print("0")
+        pass
+
+    except KeyboardInterrupt:
+        print("\nProgram Exitted Succesfully")
+        break
+    
+    else:
+        if x <= y:
+            fraction = round((x / y) * 100)
+            if fraction >= 99:
+                print("F")
+            elif fraction <= 1:
+                print("E")
+            else:
+                print(f"{fraction}%")
+            break
+            
+    
