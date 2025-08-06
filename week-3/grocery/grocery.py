df4afbc3d3656c79069e32c25c73860017b78b2a
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-06 08:55:21 +0000

    Add changes to grocery.py file

--- week-3/grocery/grocery.py
+++ week-3/grocery/grocery.py
@@ -2,11 +2,21 @@ grocery = {}
 
 while True:
     try:
-        item = input().upper()
+        item = input()
+        item = item.upper()
     
-    except KeyboardInterrupt:
+    except EOFError:
         print()
         break
+    
+    if item.upper() in grocery:
+        grocery[item] += 1
+    else:
+        grocery[item.upper()] = 1
+
+for item in sorted(grocery.keys()):
+    print(grocery[item], item)
+
 
 #Allow user to fill in a list of items, one per line, until control d is pressed
 # Take the items add them to a list.
