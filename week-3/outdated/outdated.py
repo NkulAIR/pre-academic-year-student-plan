de791acc31a3e030214e28aea3f2cc76f60548c4
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-06 09:33:16 +0000

    Added psuedocode to outdated.py

--- week-3/outdated/outdated.py
+++ week-3/outdated/outdated.py
@@ -1,1 +1,7 @@
 months = {}
+
+#Turn user input to string and split year, day and month it with empty space
+#If month is not a int it raise ValueError, do the same process of splitting the string to find the month
+#Create a numbered list
+# If userdate month is in months, numbered list. return that month or its count only
+# Create a boolean variable to break out of the While loop
