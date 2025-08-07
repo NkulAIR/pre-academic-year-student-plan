580d21d875fc741d3cc01b21684bbcd3be12a729
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-07 11:54:43 +0000

    "Added complete outdated.py"

--- week-3/outdated/outdated.py
+++ week-3/outdated/outdated.py
@@ -22,11 +22,34 @@ while True:
         if user[0] in months:
             month = user[0]
             n_month = months[month]
-            day = user[1].strip(",")
+            day = int(user[1].strip(","))
             year = user[2]
-        if n_month <= 12 and day <= 31:
+            
+            if n_month <= 12 and day <= 31:
+                print(f"{year}-{n_month:02}-{day:02}")
+                break
+
+        elif user[0].isdigit:
+            user = str(*user)
+            user = user.split("/")
+            if user[0] in months:
+                continue
+            month, day, year = int(user[0]), int(user[1]), int(user[2])
+            if month <= 12 and day <= 31:
+                print(f"{year}-{month:02}-{day:02}")
+                break
+
     except TypeError:
         pass
+    
+    except ValueError:
+        month, day, year = user[0],  int(user[1]), int(user[2])
+        n_month = months[month]
+        if int(day) <= 31:
+            print(f"{year}-{n_month:02}-{day:02}")
+            break
+
+
 
 #Turn user input to string and split year, day and month it with empty space
 #If month is not a int it raise ValueError, do the same process of splitting the string to find the month
