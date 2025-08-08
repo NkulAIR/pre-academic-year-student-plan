cb6d44bb6c09db1fe2e25ffa67104b6ebc3077a4
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-08 10:37:37 +0000

    Added week-4 folder and emojize.py

---
+++ week-4/emojize/emojize.py
@@ -0,0 +1,12 @@
+import re
+import emoji
+
+def main():
+
+
+main()
+
+# Validate the string and detect the key term
+# Retrieve the emoji associated with the key term
+# Replace key term with emoji
+
