8780e8c2fa0dd7f189508d942fa1e8371e98ebbf
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-08 11:41:54 +0000

    Add final figlet.py file

--- week-4/figlet/figlet.py
+++ week-4/figlet/figlet.py
@@ -5,11 +5,37 @@ import random
 fonts = pyfiglet.FigletFont.getFonts()
 
 try:
-    print(sys.argv[2])
+    if len(sys.argv) >= 3 and sys.argv[2] in fonts:
+        if sys.argv[1] == "--font" or sys.argv[1] == "-f":
+            text = input("Input: ")
+            font = sys.argv[2]
+
+            custom_fig = pyfiglet.Figlet(font=sys.argv[2])
+            ascii_art = custom_fig.renderText(text)
+            print("Output: ")
+            print(ascii_art)
+        else:
+             print("Invalid usage")
+             sys.exit(1)
+    
+    elif len(sys.argv) == 1:
+        text = input("Input: ")
+        #  fonts = pyfiglet.FigletFont.getFonts()
+        random_font = random.choice(fonts)
+        custom_fig = pyfiglet.Figlet(font=random_font)
+        ascii_art = custom_fig.renderText(text)
+        print("Output: ")
+        print(ascii_art)
+
+    else:
+        print("Invalid usage")
+        sys.exit(1)
+
+
 except pyfiglet.FontNotFound:
     print("Invalid Usage")
     sys.exit(1)
 
 except IndexError:
     print("Invalid Usage")
-    sys.exit(1) add figlet
+    sys.exit(1)
