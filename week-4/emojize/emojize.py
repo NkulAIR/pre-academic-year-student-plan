7adeb031f81caba49812897279e9fe64c91235f7
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-08 11:19:07 +0000

    "Add emojize.py file"

--- week-4/emojize/emojize.py
+++ week-4/emojize/emojize.py
@@ -1,12 +1,22 @@
 import re
 import emoji
 
-def main():
+user = input("Input: ")
+print("Output:", emoji.emojize(user, language="alias"))
 
+# def main():
+#     user = input("Input: ").strip()
+#     matches = re.search(r"(^(:)(.+[\w])(:)$)", user)
+#     if matches:
+#         print(matches.group())
 
-main()
+# Function that replaces the term with the suitable emoji
+# def emoji_converter(term):
+#     match term:
+#         case ":thumbsup:" | ":thumbs_up:":
+#             return "👍"
 
+# main()
 # Validate the string and detect the key term
 # Retrieve the emoji associated with the key term
 # Replace key term with emoji
-
