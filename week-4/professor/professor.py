5ef5741b1e7cb8e78223dcb5988df7fa04354bf3
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-11 11:44:10 +0000

    Create professor.py file and folder

---
+++ week-4/professor/professor.py
@@ -0,0 +1,17 @@
+import random
+
+def main():
+#Prompt user for a number between 1-3 and reprompt if the input is invalid
+    level = get_level()
+    generate_integer(level)
+
+def get_level():
+    ...
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
