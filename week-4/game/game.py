6e0391870872a6d2556ea3b3c9817c9ec5f07205
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-11 11:26:10 +0000

    Add final game.py file

--- week-4/game/game.py
+++ week-4/game/game.py
@@ -1,37 +1,29 @@
 import random
 import sys
 
-
-
 while True:
     n = input("Level: ")
     if n.isdigit() and int(n) > 0:
         number = random.randint(1,int(n))
-
     else:
         print("Invalid level")
         continue
-
 
     while n.isdigit():
         guess = input("Guess: ")
         if guess.isdigit():
             guess = int(guess)
-            if guess == number:
-                print("Just right!")
-                sys.exit(0)
-
+            if guess <= 0:
+                continue
             elif guess > number:
                 print("Too large!")
                 continue
-
             elif guess < number:
                 print("Too small!")
                 continue
-
-        else:
-            print("Invalid guess")
-            continue
+            else:
+                print("Just right!")
+                sys.exit(0)
 
 # Prompt user for a level, validate the input make sure it is a postive integer
 # Generate random number between 1 and range
