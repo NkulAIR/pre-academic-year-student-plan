d672bfb2c77675b836b4e3d992561bd3a953f2d3
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-13 11:17:20 +0200

    Update professor.py

--- week-4/professor/professor.py
+++ week-4/professor/professor.py
@@ -1,68 +1,66 @@
 import random
-
+import sys
 
 def main():
+    score = 0
+    tries = 0
+
     level = get_level()
-    generate_integer(level)
+    for i in range(10):
+        a = generate_integer(level)
+        b = generate_integer(level)
+
+        while tries < 3:
+            try:
+                print(f"{a} + {b} =", end=" ")
+                answer = int(input())
+                if answer == a + b:
+                    score += 1
+                    break
+                else:
+                    tries += 1
+                    print("EEE")
+                    continue
+
+
+            except ValueError:
+                print("EEE")
+                continue
+        else:
+            print(f"{a} + {b} = {a + b}")
+            tries = 0
+    print("Score: ", score)
+    sys.exit()
 
 
 def get_level():
 #Prompt user for a number between 1-3 and reprompt if the input is invalid
     while True:
-        level = input("Level: ")
-        if level.isdigit() == True:
-            if 1 <= int(level) <= 3:
-                return int(level)
-
+        try:
+            level = int(input("Level: "))
+            if level >= 1 and level <= 3:
+                return level
+            else:
+                continue
+        except ValueError:
+            continue
 
 def generate_integer(level):
-# Generates random sums structured as X+Y if level is 1 digit and XX + YY if level is 2
-    score = 0
-    numbers = []
-    for i in range(10):
-        match level:
-            case 1:
-                a = random.randint(0,9)
-                b = random.randint(0,9)
-                # numbers.append(a)
-                # numbers.append(b)
-            case 2:
-                a = random.randint(10,99)
-                b = random.randint(10,99)
-            case 3:
-                a = random.randint(100,999)
-                b = random.randint(100,999)
-            case _:
-                raise ValueError
+    match level:
+        case 1:
+            a = random.randint(0,9)
+        case 2:
+            a = random.randint(10,99)
+        case 3:
+            a = random.randint(100,999)
+        case _:
+            raise ValueError
 
-        tries = 0
-        while tries < 3:
-            try:
-                print(f"{a} + {b} = ", end="")
-                answer = int(input())
-                if a + b == answer:
-                    score += 1
-                    break
-                elif answer != a + b and tries < 2:
-                    print("EEE")
-                    tries += 1
-                else:
-                    print("EEE")
-                    print(f"{a} + {b} = {a + b}")
-                    break
-
-            except ValueError:
-                tries += 1
-                print("EEE")
-        else:
-            print(f"{a} + {b} = {a + b}")
-            break
-
-    print("Score: ",score)
-    # print(numbers)
-
+    return a
 
 
 if __name__ == "__main__":
     main()
+
+
 
