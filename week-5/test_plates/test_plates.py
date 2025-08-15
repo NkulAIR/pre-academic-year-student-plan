82433e5b4f01caa407121e09d2a4faccb8248825
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-15 13:32:18 +0000

    Add tested test_plates.py file

--- week-5/test_plates/test_plates.py
+++ week-5/test_plates/test_plates.py
@@ -1,27 +1,30 @@
 import pytest
 from plates import is_valid
 
-def main():
-    test_plates()
+def test_order():
+    assert is_valid("AA") == True
+    assert is_valid("0A") == False
 
+def test_alphabetical():
+    assert is_valid("20") == False
+    assert is_valid("0A") == False
+    assert is_valid("2") == False
 
 def test_numplace():
     assert is_valid("AAA222") == True
     assert is_valid("AAA22A") == False
 
-def test_space():
-    assert is_valid("AA") == True
-    assert is_valid("A A") == False
-
 def test_length():
-    assert is_valid("AAAAAA") == False
     assert is_valid("AAAAAAA") == False
+    assert is_valid("AAA") == True
+    assert is_valid("A") == False
 
 def test_zeroplace():
-    assert is_valid("CS50") == True
-    assert is_valid("CS05") == False
+    assert is_valid("AA200") == True
+    assert is_valid("AAA022") == False
+    assert is_valid("0AA") == False
 
 def test_alphanum():
     assert is_valid("AA22") == True
-    # assert is_valid("AA 22") == False
+    assert is_valid("AA 22") == False
     assert is_valid("AA@22") == False
