81df0f7b2ca3138483d40af7cf9de3a3171c5bc2
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-18 12:20:18 +0000

    Add fuel.py file

--- week-5/test_fuel/fuel.py
+++ week-5/test_fuel/fuel.py
@@ -1,34 +1,60 @@
 def main():
-    fuel = input("")
-    percent = convert(fuel)
-    print(gauge(percent))
+    while True:
+        try:
+            fraction = input("Fraction: ")
+            if "/" in fraction and fraction.count("/") == 1:
+                percentage = int(convert(fraction))
+                # print(convert(fraction))
+                print(gauge(percentage))
+                break
+
+        # except ValueError:
+        #     continue
+        except TypeError:
+            continue
+
 
 
 def convert(fraction):
-    if "/" in fraction:
-        if fraction[0].isdigit():
-            if fraction[-1].isdigit():
-                if fraction[0] > fraction[-1]:
-                    raise ValueError("")
-                elif fraction[-1] == 0:
-                    raise ZeroDivisionError("")
-                else:
-                    x, y = int(fraction[0]), int(fraction[-1])
-                    p = round((x / y) * 100)
-                    return p
-                
-            raise ZeroDivisionError("Y")
-        raise ValueError("X")
+    fraction = fraction.split("/")
+    x,y = int(fraction[0]), int(fraction[1])
+    if x < 0 or y < 0:
+        raise ValueError("Negative")
+
+    if x <= y:
+        percentage = int(round((x / y) * 100))
+        return percentage
+
+
+    # else:
+
+    #     elif y == 0:
+    #         raise ZeroDivisionError
+
+    #     elif x and y != int() or x > y:
+    #         raise ValueError
+
 
 
 def gauge(percentage):
-    if int(percentage) <= 1:
-        return "E"
-    if int(percentage) >= 99:
-        return "F"
+
+    try:
+        percentage = int(percentage)
+    except TypeError:
+        raise ValueError
     else:
-        return f"{percentage}%"
+        if percentage == int(percentage):
+            if percentage >= 99:
+                return "F"
+
+            elif percentage <= 1:
+                return "E"
+            else:
+                return f"{percentage}%"
+        else:
+            raise ValueError
 
 
 if __name__ == "__main__":
     main()
+
