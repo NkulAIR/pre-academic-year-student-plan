af098f8b4744d5972c081f3db4e34a66e4f2f053
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-18 10:02:08 +0000

    Add fuel.py file

--- week-5/test_fuel/fuel.py
+++ week-5/test_fuel/fuel.py
@@ -1,21 +1,33 @@
 def main():
-    print(convert("1/2"))
+    fuel = input("")
+    percent = convert(fuel)
+    print(gauge(percent))
 
 
 def convert(fraction):
     if "/" in fraction:
         if fraction[0].isdigit():
             if fraction[-1].isdigit():
-                try:
+                if fraction[0] > fraction[-1]:
+                    raise ValueError("")
+                elif fraction[-1] == 0:
+                    raise ZeroDivisionError("")
+                else:
                     x, y = int(fraction[0]), int(fraction[-1])
-                    return(x,y)
-                except ValueError("Not an integer")
+                    p = round((x / y) * 100)
+                    return p
                 
-
+            raise ZeroDivisionError("Y")
+        raise ValueError("X")
 
 
 def gauge(percentage):
-    ...
+    if int(percentage) <= 1:
+        return "E"
+    if int(percentage) >= 99:
+        return "F"
+    else:
+        return f"{percentage}%"
 
 
 if __name__ == "__main__":
