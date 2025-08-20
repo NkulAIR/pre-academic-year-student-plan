7650af8a3f8c4e98ee0a72dfcc0a73770b72d57a
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-20 12:42:39 +0000

    Update interpreter.py file

--- week-1/interpreter/interpreter.py
+++ week-1/interpreter/interpreter.py
@@ -1,24 +1,18 @@
-import operator
-
-user = (input("Expression:")).strip(" ")
+user = input("Expression: ")
 expression = user.split(" ")
 x = int(expression[0])
 y = (expression[1])
 z = int(expression[2])
 
-if y == "+":
-    result = operator.add(x, z)
-    print(float(result))
-elif y == "-":
-    result = operator.sub(x, z)
-    print(float(result))
-elif y == "*":
-    result = operator.mul(x, z)
-    print(float(result))
-elif y == "/":
-    result = operator.truediv(x, z)
-    print(float(result))
+match y:
+    case "+":
+        result = x + z
+    case "-":
+        result = x - z
+    case "*":
+        result = x * z
+    case "/":
+        result = x / z
 
-
-
+print(float(result))
 
