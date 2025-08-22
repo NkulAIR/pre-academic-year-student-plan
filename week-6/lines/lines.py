e43404919372af43f62c8b3a85af58db0b100562
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-22 13:34:12 +0000

    Add lines directory and week-6 README.md

---
+++ week-6/lines/lines.py
@@ -0,0 +1,31 @@
+import sys
+
+try:
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    if sys.argv[1].endswith(".py"):
+        # file = sys.argv[1]
+        with open(sys.argv[1]) as file:
+            ...
+    else:
+        sys.exit("Not a Python file")
+except IndexError:
+    sys.exit("Too few command-line arguments")
+
+# count = 0
+
+# if len(sys.argv) > 1:
+#     sys.exit("Too many command-line arguments")
+# if len(sys.argv) <= 0:
+#     sys.exit("Too few command-line arguments")
+# else:
+#     if sys.argv[0]:
+#         with open(sys.argv[0]) as file:
+#             for line in file:
+#                 if line.startswith("#") or not line:
+#                     pass
+#                 else:
+#                     count += 1
+#         print(count)
+#     else:
+#         sys.exit("File does not exist")
