1b87ed2176515e48066395ec3edbe19ac9dd28b8
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-25 13:03:06 +0000

    Update lines.py file

--- week-6/lines/lines.py
+++ week-6/lines/lines.py
@@ -5,8 +5,19 @@ try:
         sys.exit("Too many command-line arguments")
     if sys.argv[1].endswith(".py"):
         # file = sys.argv[1]
-        with open(sys.argv[1]) as file:
-            ...
+        try:
+            with open(sys.argv[1]) as file:
+                lines = file.readlines()
+                # count = 0
+                # for line in file:
+                #     if line.startswith("#") or not line:
+                #         pass
+                #     else:
+                #         count += 1
+            print(lines)
+
+        except FileNotFoundError:
+            sys.exit("File does not exist")
     else:
         sys.exit("Not a Python file")
 except IndexError:
