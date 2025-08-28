8aa96bf2d70fe9f53e2524fe2de3ea0cdf378a72
Nkululeko Tevin Tshaka <148770997+NkulAIR@users.noreply.github.com>
2025-08-28 12:11:24 +0000

    Add scourgify.py file and csv files

--- week-6/scourgify/scourgify.py
+++ week-6/scourgify/scourgify.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 
 if len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
@@ -7,8 +8,28 @@ elif len(sys.argv) < 3:
 else:
     file_name = sys.argv[1]
     try:
-        with open(file_name) as csvfile:
-            #Write a new file
+        file_name1 = file_name
+        file_name2 = sys.argv[2]
+
+        with open(file_name1) as csvfile:
+            reader = csv.DictReader(csvfile)
+            data = [row for row in reader]
+        
+        if sys.argv[2].endswith(".csv"):
+            with open(file_name2, "w") as file:
+                f_names = ["first", "last", "house"]
+                writer = csv.DictWriter(file, fieldnames= f_names)
+                writer.writeheader()
+
+                for info in data:
+                    name = info["name"] 
+                    house = info["house"] 
+
+                    last, first = name.split(", ")
+                    writer.writerow({"first": first.strip(), "last": last.strip(), "house": house})
+        else:
+            sys.exit("Invalid file name") 
+
     except FileNotFoundError:
         sys.exit(f"Could not read {file_name}")
 
